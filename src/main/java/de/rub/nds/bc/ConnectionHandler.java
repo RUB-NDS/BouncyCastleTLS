1a6da465fba8507427df90c6dca109eb2b7e5299
Nurullah Erinola <nurullah.erinola@rub.de>
2025-01-31 15:23:58 +0100

    Cleaned up

--- src/main/java/de/rub/nds/bc/ConnectionHandler.java
+++ src/main/java/de/rub/nds/bc/ConnectionHandler.java
@@ -5,49 +5,41 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.Socket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.crypto.tls.TlsServerProtocol;
-
+import org.bouncycastle.tls.TlsServerProtocol;
 
 public class ConnectionHandler implements Runnable {
 
-    private final static Logger LOGGER = LogManager.getLogger(ConnectionHandler.class);
+    private final static Logger LOGGER = LogManager.getLogger();
 
     private final TlsServerProtocol tlsServerProtocol;
 
-    /**
-     * ConnectionHandler constructor
-     * 
-     * @param socket
-     *            - The socket of the connection
-     */
-    public ConnectionHandler(final TlsServerProtocol tlsServerProtocol) {
+    public ConnectionHandler(TlsServerProtocol tlsServerProtocol) {
 	this.tlsServerProtocol = tlsServerProtocol;
     }
 
     @Override
     public void run() {
-
 	LOGGER.debug("new Thread started");
 
 	try {
 	    final BufferedReader br = new BufferedReader(new InputStreamReader(tlsServerProtocol.getInputStream()));
 	    final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(tlsServerProtocol.getOutputStream()));
 	    String line = "";
+            
 	    while ((line = br.readLine()) != null) {
 		LOGGER.debug(line);
 		bw.write("ack");
 		bw.flush();
 	    }
-	} catch (IOException e) {
-	    LOGGER.debug(e.getLocalizedMessage(), e);
+	} catch (IOException ex) {
+	    LOGGER.debug(ex.getLocalizedMessage(), ex);
 	} finally {
 	    try {
 	    tlsServerProtocol.close();
-	    } catch (final IOException ioe) {
-		LOGGER.debug(ioe.getLocalizedMessage(), ioe);
+	    } catch (final IOException ex) {
+		LOGGER.debug(ex.getLocalizedMessage(), ex);
 	    }
 	}
     }
